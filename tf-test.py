88f99f1e24ba543db64ad228fc90789adb7a612d
Luisx09 <lrodrigue@ltu.edu>
2019-11-22 20:39:45 -0500

    Updated to use keras to download American dataset

--- tf-test.py
+++ tf-test.py
@@ -7,12 +7,18 @@ from tensorflow import keras
 # Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
+data_dir = tf.keras.utils.get_file(origin='http://cvrr.ucsd.edu/LISA/Datasets/signDatabasePublicFramesOnly.zip',
+                                         fname='LISA_set', untar=True)
+import pathlib
+data_dir = pathlib.Path(data_dir)
 
+#data_dir = tf.keras.utils.get_file()
 #print(tf.__version__)
 #path = r'/C:/Users/Splinter/Pictures/GTSRB/Final_Training/Images'
+#data_dir = pathlib.Path("C:/Users/Splinter/Pictures/")
 
-list_ds = tf.data.Dataset.list_files('~/Pictures/GTSRB/Final_Training/Images/*/*.ppm')
+list_ds = tf.data.Dataset.list_files(str(data_dir/'*'))
 
 #print (list_ds)
 for f in list_ds.take(5):
-  print(f.values)
+  print(f.numpy())

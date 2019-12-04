d991f83c2ab4dacceee74ab1be58bbbdc93eca05
Luisx09 <lrodrigue@ltu.edu>
2019-12-04 14:52:30 -0500

    Updated to load and run saved models against a testing set.

--- use_latest_saved_mod.py
+++ use_latest_saved_mod.py
@@ -14,6 +14,13 @@ import os
 import re
 import pathlib
 
+# Should match image dimensions in CNN initial layer
+IMG_WIDTH = 32
+IMG_HEIGHT = 32
+
+# Should be a common divisor of both sets to 
+BATCH_SIZE = 70
+
 model_dir = "/tmp/traffic_sign/"
 model_dir = pathlib.Path(model_dir)
 models_made = np.array([item.name for item in model_dir.glob('*')]).astype(np.float)
@@ -22,4 +29,29 @@ mod_to_use = input("Which model version to use? ")
 if round(float(mod_to_use),1) in models_made:
     latest_mod = mod_to_use
 
-loaded = tf.saved_model.load(str(model_dir/str(latest_mod)))
+loaded = tf.keras.models.load_model(str(model_dir/str(latest_mod)))
+
+data_dir = r'.'
+test_data_dir = data_dir + '\TestingDataNoise'
+test_data_dir = pathlib.Path(test_data_dir)
+
+CLASS_NAMES = np.array([item.name for item in test_data_dir.glob('*')])
+print (CLASS_NAMES)
+
+test_image_count = len(list(test_data_dir.glob('*/*.jpg')))
+TEST_STEPS_PER_EPOCH = np.ceil(test_image_count/BATCH_SIZE)
+
+# The 1./255 is to convert from uint8 to float32 in range [0,1].
+image_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+test_data_gen = image_generator.flow_from_directory(directory=str(test_data_dir),
+                                                     batch_size= BATCH_SIZE,
+                                                     shuffle=True,
+                                                     target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                     classes = list(CLASS_NAMES),
+                                                     class_mode = 'sparse')                                                     
+
+image_test, label_test= next(test_data_gen)
+
+test_loss, test_acc = loaded.evaluate(image_test,  label_test, verbose=2)
+
+print(test_acc)

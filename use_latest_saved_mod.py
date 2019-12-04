aae3388ca62adcc99ea2bd551a7db4806b864881
Luisx09 <lrodrigue@ltu.edu>
2019-12-04 18:01:13 -0500

    Added ability to run prediction on a single image on pre-trained model.
    Gives output of figure w/caption.

--- use_latest_saved_mod.py
+++ use_latest_saved_mod.py
@@ -25,9 +25,10 @@ model_dir = "/tmp/traffic_sign/"
 model_dir = pathlib.Path(model_dir)
 models_made = np.array([item.name for item in model_dir.glob('*')]).astype(np.float)
 print(models_made)
-mod_to_use = input("Which model version to use? ")
-if round(float(mod_to_use),1) in models_made:
-    latest_mod = mod_to_use
+#mod_to_use = input("Which model version to use? ")
+#if round(float(mod_to_use),1) in models_made:
+#    latest_mod = mod_to_use
+latest_mod = 1.0
 
 loaded = tf.keras.models.load_model(str(model_dir/str(latest_mod)))
 
@@ -55,3 +56,28 @@ image_test, label_test= next(test_data_gen)
 test_loss, test_acc = loaded.evaluate(image_test,  label_test, verbose=2)
 
 print(test_acc)
+
+#img_file = tf.keras.utils.get_file('TestingData/stopAhead/5928_stopAhead_1323819280.avi_image23.png', '.')
+img_file = 'TestingData/stopAhead/5928_stopAhead_1323819280.avi_image23.png'
+
+img = tf.keras.preprocessing.image.load_img(img_file, target_size=[32, 32])
+plt.imshow(img)
+plt.axis('off')
+x = tf.keras.preprocessing.image.img_to_array(img)
+x = tf.keras.applications.mobilenet.preprocess_input(
+    x[tf.newaxis,...])
+
+predictions = loaded.predict(tf.constant(x))
+
+#decoded = imagenet_labels[np.argsort(labeling)[0,::-1][:5]+1]
+#plot_value_array(1, predictions[0], label_test)
+#_ = plt.xticks(range(10), CLASS_NAMES, rotation=45)
+
+#np.argmax(predictions[0])
+
+predicted_label = np.argmax(predictions[0])
+
+print("Prediction on image:\n", CLASS_NAMES[predicted_label])
+
+plt.text(16 ,33,CLASS_NAMES[predicted_label])
+plt.show()
